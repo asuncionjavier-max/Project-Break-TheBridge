34c4eeda5b8afc898446792725185d0db98f85aa
Javi <asuncionjavier96@gmail.com>
2026-04-27 12:01:53 +0200

    añadimos el randomizador de imagenes y empezamos con el generador de contraseñas

---
+++ JS/main.js
@@ -0,0 +1,21 @@
+const body = document.body
+const imagenes = [
+    '/ASSETS/machine-_-LhNDEs4MP5w-unsplash.jpg',
+    '/ASSETS/brian-mcgowan-nqX-s99wSvI-unsplash.jpg',
+    '/ASSETS/evgeniya-pron-aW_hvFsBlXs-unsplash.jpg',
+    '/ASSETS/flyd-3f_knnjHFIg-unsplash.jpg',
+    '/ASSETS/harrison-qi-ce-K_rj1P7U-unsplash.jpg',
+    '/ASSETS/logan-voss-TlsvyNK5s98-unsplash.jpg',
+    '/ASSETS/nasa-Q1p7bh3SHj8-unsplash.jpg',
+    '/ASSETS/pawel-nolbert-4u2U8EO9OzY-unsplash.jpg',
+    '/ASSETS/vishnu-mohanan-pfR18JNEMv8-unsplash.jpg',
+    '/ASSETS/walkator-klMii3cR9iI-unsplash.jpg',
+]
+
+function randomizeImg(){
+const imgRandom = Math.floor(Math.random()*imagenes.length)
+const imagenSelect = imagenes[imgRandom]
+body.style.backgroundImage = `url(${imagenes[imgRandom]})`
+ }
+setInterval(randomizeImg, 15000)
+
